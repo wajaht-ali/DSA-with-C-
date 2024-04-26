90dd34967460b6987a8e7e808bfae6593181fa64
wajaht-ali <contact.chwajahat@gmail.com>
2024-04-26 17:20:56 +0500

    String's common methods

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -1,12 +1,17 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main() {
-    string s;
-    cout<<"Enter your string: ";
-    cin>>s;
-    cout<<endl;
-    cout<<"Your string is: "<<s;
+int main()
+{
+    string s = "Wajahat Ali";
+
+    cout << "Your string is: " << s << endl;
+
+    // some of the commonly used methods of string
+    cout << s.length() << endl; // OR   s.size()
+    cout << s.empty() << endl;
+    s.push_back('w');
+    cout << s << endl;
+    s.erase();          //erase the whole string.
     return 0;
 }
